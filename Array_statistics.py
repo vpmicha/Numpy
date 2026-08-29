75bec99171145ad4c3b30fd602cdcbba80ab43e1
vpmicha <vpmicha@gmail.com>
2026-08-29 20:27:41 +0300

    Fixed small bugs in Array statistics

--- Array_statistics.py
+++ Array_statistics.py
@@ -1,16 +1,15 @@
 import numpy as np
 
-scores = np.array([91,95,34,65,18,54,87,43,100,91])
+scores = np.array([91,95,34,65,18,54,87,43,100,91,76,43,67,87,46])
 
-print(f'MinScore: {np.argmin(scores)}')
-print(f'MaxScore: {np.argmax(scores)}')
-print(f'Mean: {np.mean(scores)}')
+print(f'MinScore: {np.min(scores)}')
+print(f'MaxScore: {np.max(scores)}')
+print(f'Mean: {round(np.mean(scores), 2)}')
 print(f'Median: {np.median(scores)}')
-print(f'Mean: {np.mean(scores)}')
 print(f'Standard deviation: {round(np.std(scores), 2)}')
 
 passed = scores >= 50
-passing_scores = np.where(scores>=50, scores, 'Fail')
+passing_scores = scores[scores >= 50]
 print(passed)
 print(passing_scores)
 
